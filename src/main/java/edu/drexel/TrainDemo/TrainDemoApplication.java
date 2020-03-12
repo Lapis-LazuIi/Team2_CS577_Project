67f68ae48ea9ade416674d78d98152bfae69ec84
Preethi Prabhakar <pprabhakar27@dxc.com>
2020-03-11 22:01:58 -0400

    Initial commit - search basic

--- src/main/java/edu/drexel/TrainDemo/TrainDemoApplication.java
+++ src/main/java/edu/drexel/TrainDemo/TrainDemoApplication.java
@@ -2,10 +2,17 @@ package edu.drexel.TrainDemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TrainDemoApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(TrainDemoApplication.class, args);
-    }
+public class TrainDemoApplication extends SpringBootServletInitializer {
+	public static void main(String[] args) {
+		SpringApplication.run(TrainDemoApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TrainDemoApplication.class);
+	}
 }
